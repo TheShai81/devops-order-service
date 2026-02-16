2959163eedb061b2c38e8dbaa53d7c2b3c9b3c95
TheShai81 <srbolduc76@gmail.com>
2026-02-16 17:51:05 -0600

    Added CORS capabilitity so frontend can reach it

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from .routes import order_bp
 
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.register_blueprint(order_bp)
     return app
